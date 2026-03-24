9ae6e4a1d70688fd0441a140e786e5c4e0e46d2e
KJM <kunalmahajan0202@gmail.com>
2026-03-24 13:31:38 +0530

    conftest.py updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,15 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pytest
+from db.session import Base, engine
+
+
+import db.models 
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield
